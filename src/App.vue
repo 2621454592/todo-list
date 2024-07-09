21eed4df9dd25951010faa6d130c0c5ffb117333
lipenghui <2621454592@qq.com>
2024-07-09 14:50:58 +0800

    初始化todolist

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader />
+      <TodoList :todos="todos" :deleteTodo="deleteTodo"/>
+      <TodoFooter />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoList,
+    TodoFooter
+  },
+  data() {
+    return {
+      todos:[
+        {id:'001',title:'吃饭',done:true},
+        {id:'002',title:'睡觉',done:false},
+        {id:'003',title:'打豆豆',done:true}]
+    }
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index,1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*app*/
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import '../src/assets/css/base.css';
+//引入Vue
+//import Vue from 'vue'
 createApp(App).mount('#app')
+
+// new Vue({
+//     el: '#app',
+//     render: h => h(App)
+// })
