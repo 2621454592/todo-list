f931a0acbbcc430665824ad815f016e5662d54d9
lph <2621454592@qq.com>
2024-07-17 23:14:18 +0800

    关闭lint验证

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,56 +1,62 @@
 <template>
-    <div>
-        <li :style="{background:bgColor}" @mouseenter="handleEnter(false)">
-            <label>
-                <input type="checkbox" :checked="todo.done" @change="change(todo.id)"/>
-                <span>{{ todo.title }}</span>
-            </label>
-            <button class="btn btn-danger" v-show="isShow" @click="deleteItem()">删除</button>
-        </li>
-    </div>
+  <div>
+    <li
+      :style="{ background: bgColor }"
+      @mouseenter="handleEnter(false)"
+      @mouseleave="handleEnter(true)"
+    >
+      <label>
+        <input type="checkbox" :checked="todo.done" @change="change(todo.id)" />
+        <span>{{ todo.title }}</span>
+      </label>
+      <button class="btn btn-danger" v-show="isShow" @click="deleteItem()">
+        删除
+      </button>
+    </li>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TodoItem",
-        data() {
-            return {
-                isShow:false,
-                bgColor:'white'
-            }
-        },
-        props: {
-            todo: {
-                type: Object
-            },
-            deleteTodo: {
-                type: Function
-            },
-            index: {
-                type: Number
-            },
-            updateChecked:{
-                type:Function
-            }
-        },
-        methods: {
-            deleteItem() {
-                const {index,todo,deleteTodo}=this;
-                if(window.confirm(`确定删除${todo.title}吗？`)){
-                    deleteTodo(index);
-                }
-            },
-            handleEnter(isEnter){
-                if(isEnter){
-                    this.isShow=true;
-                    this.bgColor='#aaa';
-                }
-            },
-            change(id){
-                this.updateChecked(id);
-            }
-        }
-    }
+export default {
+  name: "TodoItem",
+  data() {
+    return {
+      isShow: false,
+      bgColor: "white",
+    };
+  },
+  props: {
+    todo: {
+      type: Object,
+    },
+    deleteTodo: {
+      type: Function,
+    },
+    index: {
+      type: Number,
+    },
+    updateChecked: {
+      type: Function,
+    },
+  },
+  methods: {
+    deleteItem() {
+      const { index, todo, deleteTodo } = this;
+      if (window.confirm(`确定删除${todo.title}吗？`)) {
+        deleteTodo(index);
+      }
+    },
+    handleEnter(isEnter) {
+      if (isEnter) {
+        this.isShow = true;
+        this.bgColor = "#aaa";
+      }
+    },
+    change(id) {
+      this.updateChecked(id);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -88,10 +94,10 @@ li:before {
 li:last-child {
   border-bottom: none;
 }
-li:hover{
+li:hover {
   background-color: #dddddd;
 }
-li:hover button{
+li:hover button {
   display: block;
 }
 </style>
